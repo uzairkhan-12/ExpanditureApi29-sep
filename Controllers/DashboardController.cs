ff7e39e03e3434b5eb3ba263b90641066ab910b1
Ab <Ab@DESKTOP-4F514TS>
2020-10-01 19:33:34 +0500

    Dashboard controller and action creation

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+using ExpanditureApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace ExpanditureApi29_sep.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController:ControllerBase
+    {
+        private ExpanditureContext db;
+
+    public DashboardController(ExpanditureContext context){
+        db=context;
+    }
+
+        [HttpGet]
+        [Route("get-income")]
+        public IActionResult GetIncome(){
+            return Ok(db.Income.Sum(x=>x.amount));
+        }
+    }
+}
--- Models/Income.cs
+++ Models/Income.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ExpanditureApi.Models {
     public class Income {
         public int Id {get ; set ;}
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal amount { get ; set ;}
     }
 }
