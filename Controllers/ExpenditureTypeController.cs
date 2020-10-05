8f97456a5aa348ee6f0b09af7b89d27206f1099c
Ab <Ab@DESKTOP-4F514TS>
2020-10-05 19:24:22 +0500

    Endpoint for updating expandituretype

--- Controllers/ExpenditureTypeController.cs
+++ Controllers/ExpenditureTypeController.cs
@@ -8,29 +8,43 @@ namespace ExpanditureApi29_sep.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ExpenditureTypeController:ControllerBase
+    public class ExpenditureTypeController : ControllerBase
     {
-    private ExpanditureContext db;
-public ExpenditureTypeController(ExpanditureContext context){
-    db=context;
-}
+        private ExpanditureContext db;
+        public ExpenditureTypeController(ExpanditureContext context)
+        {
+            db = context;
+        }
 
-    [HttpPost]
-    [Route("add")]
-    public IActionResult Add([FromBody]ExpenditureType expenditureType){
-        expenditureType.AddedOn=DateTime.Now;
-        expenditureType.IsActive=true;
-        db.ExpenditureTypes.Add(expenditureType);
-        db.SaveChanges();
-        return Ok(expenditureType);
-    }
-    
-    [HttpGet]
-    [Route("get-types")]
-    public IActionResult Get(){
-        return Ok(db.ExpenditureTypes.ToList());
-    }
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add([FromBody] ExpenditureType expenditureType)
+        {
+            expenditureType.AddedOn = DateTime.Now;
+            expenditureType.IsActive = true;
+            db.ExpenditureTypes.Add(expenditureType);
+            db.SaveChanges();
+            return Ok(expenditureType);
+        }
 
+        [HttpGet]
+        [Route("get-types")]
+        public IActionResult Get()
+        {
+            return Ok(db.ExpenditureTypes.ToList());
 
+        }
+        [HttpGet]
+        [Route("toggle-type")]
+        public IActionResult ToggleType([FromQuery] int typeId)
+        {
+            var row = db.ExpenditureTypes.FirstOrDefault(x => x.Id == typeId);
+            if (row != null)
+            {
+                row.IsActive = !row.IsActive;
+            }
+            db.SaveChanges();
+            return Ok(true);
+        }
     }
 }
